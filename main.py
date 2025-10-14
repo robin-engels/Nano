1df98cb15bddfa8d53e7c808e9399e1143f7e82e
robin-engels~ <rbnengels@gmail.com>
2025-10-14 21:25:02 +0200

    initial

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-class APP:
-    def __init__(self):
-        self.name = "My Application"
-        self.version = "1.0.0"
-
-    def run(self):
-        print(f"Running {self.name} version {self.version}")
+print("initial commit")
